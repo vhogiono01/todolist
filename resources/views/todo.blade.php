a53264726e5411423afc66660ebb954bc1731aa6
Vigor Ho <vhogiono01@student.ciputra.ac.id>
2017-03-13 11:37:15 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ToDo List</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    
+    </head>
+    <body>
+    <h1>ToDo List</h1>
+        <ul>
+        <li>Taki</li>
+        <li>Mitsuha</li>
+        <li>Yotsuha</li>
+        </ul>
+        <form>
+            <input type="text" name="todo_item">
+            <button type="submit">Add</button>
+        </form>
+    </body>
+</html>
